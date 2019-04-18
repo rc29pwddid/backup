61fa9693a51a1ba952fe8e435b2c12e15ba8b1ff
Budi Santoso <42322092+rc29pwddid@users.noreply.github.com>
2019-04-18 18:51:16 +0700

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TODAY=’/bin/date +%y%m%d-%H’
+
+FILENAME=”FULLBACKUP-${TODAY}”
+
+tar -cvpf /home/${FILENAME}.tar –directory=/ –exclude=proc –exclude=sys –exclude=dev/pts –exclude=backups .
+
+exit 0
