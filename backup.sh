7e9bb446a50c85bc3c0b47cf0a0e2a5aa45dc3b6
Budi Santoso <42322092+rc29pwddid@users.noreply.github.com>
2019-05-20 02:55:34 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 TODAY=’/bin/date +%y%m%d-%H’
-
 FILENAME=”FULLBACKUP-${TODAY}”
 
 tar -cvpf /home/${FILENAME}.tar –directory=/ –exclude=proc –exclude=sys –exclude=dev/pts –exclude=backups .
